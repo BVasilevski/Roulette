c5172176f28b8739848d164552a041351512d975
BVasilevski <thegamer3212@gmail.com>
2023-07-05 11:15:08 +0200

    Final version

--- Roulette/Form1.cs
+++ Roulette/Form1.cs
@@ -4,16 +4,28 @@ using static System.Runtime.InteropServices.JavaScript.JSType;
 using System.Runtime.InteropServices;
 using System.ComponentModel;
 
+// ...
+
+
+
 namespace Roulette
 {
-    public partial class MainForm : Form
+
+    public partial class Form1 : Form
     {
+
+        //TODO: STAVI ZA BRISENJE NA BETOVITE
         private Image wheelImage;
+        private float rotationAngle = 0;
+        private const float rotationSpeed = 2f;
         private Ball ball;
         private Betting Betting;
+        private int ticks = 0;
         private Random random = new Random();
-        private List<int> tickValues = new List<int>();
-        private Dictionary<int, int> numbers = new Dictionary<int, int>()
+        List<int> tickValues = new List<int>();
+        int drawnNumber;
+        int duration;
+        Dictionary<int, int> numbers = new Dictionary<int, int>()
         {
             { 598, 28 },
             { 593, 7 },
@@ -26,7 +38,7 @@ namespace Roulette
             { 559, 20 },
             { 554, 16 },
             { 549, 33 },
-            { 544, 15 },
+            { 544, 16 },
             { 540, 24 },
             { 535, 5 },
             { 530, 10 },
@@ -53,44 +65,174 @@ namespace Roulette
             { 428, 35 },
             { 423, 12 }
         };
+        List<Button> buttons = new List<Button>();
         private Wallet wallet = new Wallet(0);
         private SoundPlayer player;
 
-        private int ticks = 0;
-        private int drawnNumber;
-        private int duration;
-        public MainForm()
+        public Form1()
         {
             InitializeComponent();
             this.DoubleBuffered = true;
             this.BackColor = Color.Green;
 
             wheelImage = Image.FromFile("wheel.png");
-            wheelPictureBox.SizeMode = PictureBoxSizeMode.CenterImage;
-            wheelPictureBox.TabStop = false;
-            wheelPictureBox.BackColor = Color.Green;
-            wheelPictureBox.Size = wheelImage.Size;
-            wheelPictureBox.Image = wheelImage;
-            wheelPictureBox.Location = new Point((30), (this.ClientSize.Height - wheelPictureBox.Height) - 20);
+            pictureBox1.SizeMode = PictureBoxSizeMode.CenterImage;
+            pictureBox1.TabStop = false;
+            pictureBox1.BackColor = Color.Green;
+            pictureBox1.Size = wheelImage.Size;
+            pictureBox1.Image = wheelImage;
+            pictureBox1.Location = new Point((30), (this.ClientSize.Height - pictureBox1.Height) - 20);
 
             currentBetsList.BackColor = Color.Green;
-            tbCurrentBalance.Text = wallet.ToString();
-
+            //GenerateButtons();
             Betting = new Betting();
             ball = new Ball(new Point(200, 65));
             tickValues = new List<int>(numbers.Keys.ToArray());
+            tbCurrentBalance.Text = wallet.ToString();
             player = new SoundPlayer("clap.wav");
+
         }
-        private void wheelPictureBox_Paint(object sender, PaintEventArgs e)
+
+        private Button GetButtonByText(string buttonText)
+        {
+            foreach (Control control in Controls)
+            {
+                if (control is Button button && button.Text == buttonText)
+                {
+                    return button;
+                }
+            }
+
+            return null; // Button with the specified text not found
+        }
+        public void GenerateButtons()
+        {
+            int x = pictureBox1.Width + 450;
+            int y = pictureBox1.Location.Y - 20;
+            int ButtonSize = 40;
+            const int ButtonSpacing = 0;
+
+            // Add the green button above buttons 1, 2, and 3
+            Button greenButton = new Button();
+            greenButton.Width = ButtonSize * 3 + ButtonSpacing * 2;
+            greenButton.Height = ButtonSize;
+            greenButton.Location = new Point(x, y);
+            greenButton.ForeColor = Color.White;
+            greenButton.Font = new Font(greenButton.Font.FontFamily, 12f, greenButton.Font.Style);
+            greenButton.Text = "0";
+            greenButton.BackColor = Color.Green;
+            Controls.Add(greenButton);
+            buttons.Add(greenButton);
+            y += ButtonSize + ButtonSpacing;
+
+            for (int i = 1; i <= 36; i++)
+            {
+                Button button = new Button();
+                button.Width = button.Height = ButtonSize;
+                button.Location = new Point(x, y);
+                button.ForeColor = Color.White;
+                button.Font = new Font(button.Font.FontFamily, 12f, button.Font.Style);
+                button.Text = i.ToString();
+
+                button.BackColor = i % 2 == 0 ? Color.Red : Color.Black;
+
+
+                Controls.Add(button);
+                buttons.Add(button);
+
+                x += ButtonSize + ButtonSpacing;
+                if (i % 3 == 0)
+                {
+                    x = pictureBox1.Width + 450;
+                    y += ButtonSize + ButtonSpacing;
+                }
+            }
+            Button button12 = GetButtonByText(15.ToString());
+
+            Button redButton = new Button();
+            redButton.Location = new Point(button12.Location.X + ButtonSize, button12.Location.Y);
+            redButton.Width = 40;
+            redButton.Height = 2 * button12.Height;
+            redButton.BackColor = Color.Red;
+            Controls.Add(redButton);
+
+            Button button21 = GetButtonByText(21.ToString());
+            Button blackButton = new Button();
+            blackButton.Location = new Point(button21.Location.X + ButtonSize, button21.Location.Y);
+            blackButton.Width = 40;
+            blackButton.Height = 2 * button12.Height;
+            blackButton.BackColor = Color.Black;
+            Controls.Add(blackButton);
+        }
+
+        private Image RotateImage(Image image, float angle)
+        {
+            // Create a new bitmap with the same size as the original image
+            Bitmap rotatedImage = new Bitmap(image.Width, image.Height);
+            rotatedImage.SetResolution(image.HorizontalResolution, image.VerticalResolution);
+
+            using (Graphics g = Graphics.FromImage(rotatedImage))
+            {
+                // Clear the background with a solid color
+                g.Clear(Color.Transparent);
+
+                // Set the interpolation mode to high quality
+                g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+                // Calculate the center point of the image
+                float centerX = image.Width / 2f;
+                float centerY = image.Height / 2f;
+
+                // Translate, rotate, and draw the image
+                g.TranslateTransform(centerX, centerY);
+                g.RotateTransform(angle);
+                g.TranslateTransform(-centerX, -centerY);
+                g.DrawImage(image, new Point(0, 0));
+            }
+
+            return rotatedImage;
+        }
+
+        private void timer1_Tick_1(object sender, EventArgs e)
+        {
+            rotationAngle += rotationSpeed;
+            rotationAngle %= 360;
+            pictureBox1.Image = RotateImage(wheelImage, rotationAngle);
+            pictureBox1.Refresh(); // Forces immediate redraw of the PictureBox
+            ball.UpdatePosition();
+        }
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            //table.drawTable(e.Graphics, pictureBox1.Location);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            ball = new Ball(new Point(200, 65));
+            if (Betting.bets.Count > 0)
+            {
+                timerBall.Start();
+                duration = tickValues[random.Next(tickValues.Count)];
+                amountNud.Value = 0;
+                ticks = 0;
+            }
+            else
+            {
+                MessageBox.Show("Please add some bets", "Add bets", MessageBoxButtons.OK);
+            }
+            pictureBox1.Invalidate();
+        }
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
         {
             ball.Draw(e.Graphics);
         }
+
         private void timerBall_Tick(object sender, EventArgs e)
         {
             ticks++;
             ball.UpdatePosition();
-            wheelPictureBox.Invalidate();
-
+            pictureBox1.Invalidate();
             if (ticks > 200)
             {
                 ball.RotationSpeed = 2f;
@@ -115,7 +257,11 @@ namespace Roulette
                 Betting.bets.Clear();
                 showCurrentBets();
             }
+
         }
+
+
+
         public void showCurrentBets()
         {
             int i = 1;
@@ -129,6 +275,12 @@ namespace Roulette
                 }
             }
         }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+
+        }
+
         private void button_Click(object sender, EventArgs e)
         {
             Button button = (Button)sender;
@@ -168,12 +320,14 @@ namespace Roulette
             }
             else
             {
-                DialogResult dg = MessageBox.Show("You don't have enough balance", "Not enough balance", MessageBoxButtons.OK);
+                DialogResult dg = MessageBox.Show("You dont have enough balance", "You dont have enough balance", MessageBoxButtons.OK);
             }
+
             updateCurrentBalance();
             showCurrentBets();
         }
-        private void buttonGreen_Click(object sender, EventArgs e)
+
+        private void button0_Click(object sender, EventArgs e)
         {
             Button button = (Button)sender;
             int number = int.Parse(button.Text);
@@ -194,12 +348,14 @@ namespace Roulette
 
             else
             {
-                DialogResult dg = MessageBox.Show("You don't have enough balance", "Not enough balance", MessageBoxButtons.OK);
+                DialogResult dg = MessageBox.Show("You dont have enough balance", "You dont have enough balance", MessageBoxButtons.OK);
             }
 
             updateCurrentBalance();
             showCurrentBets();
         }
+
+
         private void buttonRed_Click(object sender, EventArgs e)
         {
             bool contains = false;
@@ -235,13 +391,16 @@ namespace Roulette
                     DialogResult dg = MessageBox.Show("Please put amount for the bet", "Please put amount for the bet", MessageBoxButtons.OK);
                 }
             }
-            else
-            {
-                DialogResult dg = MessageBox.Show("You don't have enough balance", "Not enough balance", MessageBoxButtons.OK);
-            }
+
             updateCurrentBalance();
             showCurrentBets();
         }
+
+        private void updateCurrentBalance()
+        {
+            tbCurrentBalance.Text = wallet.ToString();
+        }
+
         private void buttonBlack_Click(object sender, EventArgs e)
         {
             bool contains = false;
@@ -279,21 +438,23 @@ namespace Roulette
             }
             else
             {
-                DialogResult dg = MessageBox.Show("You don't have enough balance", "Not enough balance", MessageBoxButtons.OK);
+                DialogResult dg = MessageBox.Show("You dont have enough balance", "You dont have enough balance", MessageBoxButtons.OK);
             }
 
             updateCurrentBalance();
             showCurrentBets();
         }
+
         private void btnDepositMoney_Click(object sender, EventArgs e)
         {
-            DepositForm form = new DepositForm();
+            Form2 form = new Form2();
             if (form.ShowDialog() == DialogResult.OK)
             {
                 wallet.addValue(form.amount);
                 updateCurrentBalance();
             }
         }
+
         private void btnRemoveLastBet_Click(object sender, EventArgs e)
         {
             if (Betting.bets.Count > 0)
@@ -303,36 +464,18 @@ namespace Roulette
             showCurrentBets();
             updateCurrentBalance();
         }
+
         private void btnRemoveAllBets_Click(object sender, EventArgs e)
         {
             Betting.RemoveAllBets(wallet);
             showCurrentBets();
             updateCurrentBalance();
         }
+
         private void btnRules_Click(object sender, EventArgs e)
         {
             RulesForm form = new RulesForm();
             form.ShowDialog();
-        }
-        private void updateCurrentBalance()
-        {
-            tbCurrentBalance.Text = wallet.ToString();
-        }
-        private void spinButton_Click(object sender, EventArgs e)
-        {
-            ball = new Ball(new Point(200, 65));
-            if (Betting.bets.Count > 0)
-            {
-                timerBall.Start();
-                duration = tickValues[random.Next(tickValues.Count)];
-                amountNud.Value = 0;
-                ticks = 0;
-            }
-            else
-            {
-                MessageBox.Show("Please place some bets", "Place bets", MessageBoxButtons.OK);
-            }
-            wheelPictureBox.Invalidate();
         }
     }
 }
