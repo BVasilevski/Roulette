44ba33a4b9bf3805e48a39c6deeb512015cd52fd
BVasilevski <thegamer3212@gmail.com>
2023-07-07 13:07:55 +0200

    Final

--- Roulette/Ball.cs
+++ Roulette/Ball.cs
@@ -31,8 +31,8 @@ namespace Roulette
         {
             RotationAngle += RotationSpeed;
             double radians = Math.PI * RotationAngle / 180;
-            float centerX = 500 / 2;  
-            float centerY = 500 / 2;  
+            float centerX = 500 / 2; //500 is pictureBox width 
+            float centerY = 500 / 2;  //500 is pictureBox height
             float newX = centerX + (float)(Math.Cos(radians) * 195);
             float newY = centerY + (float)(Math.Sin(radians) * 195);
 
